d49de15d1f73d233d7d39369770d6442a0889902
ToniRakkaen <minhkhangg32@gmail.com>
2024-12-12 22:48:21 +0700

    Create FMV

---
+++ FMV-Store/src/main/java/fmv/FMV_Store/FmvStoreApplication.java
@@ -0,0 +1,13 @@
+package fmv.FMV_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmvStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmvStoreApplication.class, args);
+	}
+
+}
---
+++ FMV-Store/src/test/java/fmv/FMV_Store/FmvStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fmv.FMV_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmvStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
